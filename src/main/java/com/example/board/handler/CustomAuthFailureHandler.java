d2927ef558d59443463e5f477d492c033450f4e7
han <rudgksdl94@gmail.com>
2024-02-07 15:54:34 +0900

    회원 가입, 회원 정보 업데이트, 회원 로그인 api

--- src/main/java/com/example/board/handler/CustomAuthFailureHandler.java
+++ src/main/java/com/example/board/handler/CustomAuthFailureHandler.java
@@ -1,10 +1,11 @@
 package com.example.board.handler;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.net.URLEncoder;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
