06e3ec5d8ad18dc065a3bc0e00a513c3a9b4a52d
han <rudgksdl94@gmail.com>
2024-02-07 15:55:24 +0900

    빠진 파일 커밋

---
+++ src/main/java/com/example/board/config/auth/PrincipalDetail.java
@@ -0,0 +1,50 @@
+package com.example.board.config.auth;
+
+import com.example.board.model.User;
+import java.util.Collection;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+@RequiredArgsConstructor
+public class PrincipalDetail implements UserDetails {
+
+  private final User user;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
+  @Override
+  public String getPassword() {
+    return null;
+  }
+
+  @Override
+  public String getUsername() {
+    return null;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return false;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return false;
+  }
+}
